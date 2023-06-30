27d5527bfee84910f370ae41cb35480ab425d8b7
gilbertsilvaa <kemegil515@gmail.com>
2023-06-30 17:30:57 -0300

    feat: remove imports unnecessary

--- mobile/lib/screens/operationForm/widgets/transfer.dart
+++ mobile/lib/screens/operationForm/widgets/transfer.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/http/dio_client.dart';
 import 'package:mobile/models/cliente.dart';
-import 'package:mobile/screens/home/home_screen.dart';
 import 'package:mobile/widgets/button.dart';
 import 'package:mobile/widgets/input.dart';
 import 'package:mobile/widgets/success.dart';
